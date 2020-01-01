ab3437a9c365aeabe08201b686a15baafd0f70d5
letelete <brunokawka@gmail.com>
2020-01-01 01:38:08 +0100

    Add text sizes

---
+++ family/lib/ui/shared/sizes.dart
@@ -0,0 +1,17 @@
+class TextSizes {
+  static const double loginWelcoming = 24.0;
+  static const double fullscreenPlaceholder = 24.0;
+
+  static const double menuContent = 36.0;
+  static const double menuCardTitle = 24.0;
+  static const double menuButton = 16.0;
+
+  static const double memberName = 16.0;
+  static const double memberPaymentInfo = 14.0;
+  static const double memberPaidStatus = 12.0;
+
+  static const double familyName = 24.0;
+  static const double familyPriceLarge = 24.0;
+  static const double familyPriceSmall = 14.0;
+  static const double familyPaymentInfo = 12.0;
+}
