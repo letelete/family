7aa8dcdc2239220496fce246ee5fea32bed4bb9e
letelete <brunokawka@gmail.com>
2020-01-01 01:39:42 +0100

    Add illustrations

--- family/lib/ui/shared/assets.dart
+++ family/lib/ui/shared/assets.dart
@@ -1,3 +1,5 @@
 class Assets {
   static const String loginPageIllustration = 'assets/illustration_login_page.svg';
+  static const String familyCoverPhotoPlaceholder = 'assets/illustration_family_placeholder.svg';
+  static const String generalNotFound = 'assets/illustration_not_found.svg';
 }
