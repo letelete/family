13acd0c59bab809830e6574ab57279b0c4a0d6d4
letelete <brunokawka@gmail.com>
2020-01-17 07:01:43 +0100

    Add FamilyCard building method via class constructor

--- family/lib/core/models/family_card.dart
+++ family/lib/core/models/family_card.dart
@@ -1,4 +1,5 @@
 import 'package:family/core/models/family.dart';
+import 'package:family/core/services/time.dart';
 
 class FamilyCard {
   final Family family;
@@ -12,4 +13,10 @@ class FamilyCard {
   })  : assert(family != null),
         assert(humanPaymentDate != null),
         assert(daysBeforePayment != null);
+
+  FamilyCard.fromFamily(Family family)
+      : family = family,
+        daysBeforePayment =
+            Time.now().durationBetween(family.paymentDay).toHuman(),
+        humanPaymentDate = family.paymentDay.toHuman();
 }
